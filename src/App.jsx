25da537e64ac3b1305184f5260dd56edc4fec32c
Vitumbiko-Kamanga <yasharkombi@gmail.com>
2026-04-04 23:57:19 +0200

    Modification wording

--- src/App.jsx
+++ src/App.jsx
@@ -8,19 +8,26 @@ import LoginScreen from './components/loginScreen'
 import Landing from './components/landing.jsx'
 import Success from './components/SuccessScreen.jsx'
 import Auth from './components/Auth.jsx'
+import Btn from './components/Btn.jsx'
+import SuccessScreen from './components/SuccessScreen.jsx'
 
 function App() {
   const [count, setCount] = useState(0)
   const fname = "vitumbiko";
   return (
-    <>
-      {/* <LoginScreen /> */}
-      {/* <Landing /> */}
-      {/* <Success /> */}
-      <Auth />
+    <div className="bg-gray-900 h-screen text-white">
+      <nav className=" flex flex-rol gap-10 px-10 py-5">
+        <Link to="/" className='text-lg'>Home</Link>
+        <Link to="/Auth" className='text-lg'>Sign In</Link>
+        <Link to="/SuccessScreen" className='text-lg'>Success screen</Link>
+      </nav>
+      <Routes>
+        <Route path='/' element= {<Landing />} />
+        <Route path='/Auth' element= {<Auth />} />
+        <Route path='/SuccessScreen' element={<SuccessScreen />} />
+      </Routes>
 
-
-    </>
+    </div>
   )
 }
 
--- src/components/landing.jsx
+++ src/components/landing.jsx
@@ -8,7 +8,7 @@ function Landing(){
             {/* <FontAwesomeIcon icon={faCheck } /> */}
             <div className='flex flex-col gap-4 justify-center pl-12'>
                 <h1 className="text-6xl font-bold align-center text-white">It's Time to make a VOTE</h1>
-                <h2 className="text-4xl text-gray-300 p-4 pl-12">Welcome to the Voting App</h2>
+                <h2 className="text-4xl text-gray-300 p-4 pl-12">Welcome to the Voting App Demo</h2>
                 <h2 className="text-2xl text-gray-300 pl-12">Cast your vote and make your voice heard</h2>
             </div>
             <Btn btnType="" text="Get Started" />
